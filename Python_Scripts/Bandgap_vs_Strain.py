f3b5ba1c696c81c170912c7c3133e0836a75958d
Abhay_Rawat <arawat1909@gmail.com>
2026-04-22 13:31:55 +0530

    Update Bandgap_vs_Strain.py

--- Python_Scripts/Bandgap_vs_Strain.py
+++ Python_Scripts/Bandgap_vs_Strain.py
@@ -7,13 +7,13 @@ bg_bisb = bg[:,1]
 bg_bias = bg[:,2]
 
 plt.figure(figsize=(8,6))
-plt.plot(x, bg_bisb, marker='s', markersize=6, color='r',
-         linewidth=2.5, markeredgecolor='black', label='BiSb')
-plt.plot(x, bg_bias, marker='o', markersize=6, color='b',
-         linewidth=2.5, markeredgecolor='black', label='BiAs')
+plt.plot(x, bg_bisb, marker='s', markersize=10, color='r',
+         linewidth=4, markeredgecolor='black', label='BiSb')
+plt.plot(x, bg_bias, marker='o', markersize=10, color='b',
+         linewidth=4, markeredgecolor='black', label='BiAs')
 
-plt.xlabel('Strain (%)', fontsize=16, fontweight='bold')
-plt.ylabel(r'Band Gap $\mathbf{E_g}$ (eV)', fontsize=16, fontweight='bold')
+plt.xlabel('Strain (%)', fontsize=18, fontweight='bold')
+plt.ylabel(r'Band Gap $\mathbf{E_g}$ (eV)', fontsize=18, fontweight='bold')
 
 plt.legend(fontsize=12)
 plt.grid(True, linestyle='--', linewidth=0.6, alpha=0.8)
